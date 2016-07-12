fdcd05145d1a73c2327d862e437bb2d69701967a
王亟亟 <526357367@qq.com>
2016-07-12 16:28:46 +0800

    day2

--- WjjPro/index.ios.js
+++ WjjPro/index.ios.js
@@ -11,35 +11,65 @@ import { AppRegistry, Text, StyleSheet,View } from 'react-native';
 class WjjPro extends Component {
 	render(){
 		return(
-			<View style={styles.container}>
-				<View style={styles.style2}>
-					<Text>Hi Wjj1!  </Text>
-					<Text>Hi Wjj2!  </Text>
-				</View>
-				<View style={[styles.style3,{flexDirection: 'column'}]}>
-					<Text style={styles.style3}>Hello Wjj1!</Text>
-					<Text style={styles.style3}>Hello Wjj2!</Text>
-				</View>
-			</View>
+		<View style={styles.style1}>
+		
+		<View style={[styles.style2,]}>
+			<Text>
+				默认位置
+			</Text>
+		</View>
+
+		<View style={[styles.style2,styles.style3]}>
+			<Text>
+				中间
+			</Text>
+		</View>
+
+		<View style={[styles.style2,styles.style4]}>
+			<Text>
+				最左边
+			</Text>
+		</View>
+
+		<View style={[styles.style2,styles.style5]}>
+			<Text>
+				最右边
+			</Text>
+		</View>
+
+		</View>
 		);
 	}
+		
 }
 
 const styles=StyleSheet.create({
-	container:{
-		flex: 1,
-	},style2:{
-		justifyContent: 'center',
-    	alignItems: 'center',
-		margin: 20,
-		flexDirection: 'row',
-		backgroundColor: '#EEFCAF',
-		flex: 2,
-	},style3:{
-		margin: 20,
-		backgroundColor: '#F5FCFF',
-		flex: 5,
-	}
+style1:{
+	flex: 1,
+	borderWidth: 1,
+	},
+style2:{
+	borderColor: 'blue',
+	borderWidth: 1,
+	width: 70,
+	height: 20,
+	},
+style3:{
+	width: 50,
+	height: 50,
+	alignSelf:'center',
+	},
+style4:{
+	width: 50,
+	height: 30,
+	alignSelf:'flex-start',
+	},
+style5:{
+	width: 50,
+	height: 25,
+	alignSelf:'flex-end',
+	},
+
 });
 
 AppRegistry.registerComponent('WjjPro', () => WjjPro);
