35d21a6ea2b3f233b534b58a10477f0bc710e83f
王亟亟 <526357367@qq.com>
2016-10-31 21:24:28 +0800

    Update main.android.js

--- main.android.js
+++ main.android.js
@@ -1,21 +1,41 @@
-import React, {Component,Navigator} from 'react';
-import {AppRegistry, View, StyleSheet, Text,} from 'react-native';
+import React, {Component, Navigator} from 'react';
+import {
+    AppRegistry,
+    View,
+    StyleSheet,
+    Text,
+    BackAndroid,
+    Platform
+} from 'react-native';
+var titleStr;
+var _navigator;
+var name;
+
+BackAndroid.addEventListener('hardwareBackPress', function() {
+    if (_navigator == null) {
+        return false;
+    }
+    if (_navigator.getCurrentRoutes().length === 1) {
+        return false;
+    }
+    if (Platform.OS === 'android') {
+        _navigator.pop();
+    }
+    return true;
+});
 
 export default class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            name: '',
-        }
-    }
-
-    componentDidMount() {
-        this.setState({name: this.props.name});
+        _navigator = this.props.navigator;
+        titleStr = this.props.titleStr;
+        name = this.props.name;
     }
     render() {
         return (
             <View>
-                <Text>获得的参数: value = {this.state.name}</Text>
+                <Text>{titleStr}
+                    页面 获得的参数: value = {name}</Text>
             </View>
         );
     }
